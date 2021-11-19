938931eb18c93c5b55e1f12ca01ba1d6ae016ec3
HuynhThanhPhong-CDTH19PMC <htphongg96@gmail.com>
2021-11-19 21:51:36 +0700

    Hoan thanh cong viec 22:00 19/11

---
+++ E_learning/database/migrations/2021_11_19_143214_create_tai_khoan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaiKhoanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tai_khoan', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('ten_dang_nhap');
+            $table->string('mat_khau');
+            $table->integer('nguoi_dung_id');
+            $table->boolean('trang_thai');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tai_khoan');
+    }
+}
