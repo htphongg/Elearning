63ba6e3b3c9772ba6e5dae04ae75ddc31f14addb
HuynhThanhPhong-CDTH19PMC <htphongg96@gmail.com>
2021-12-13 15:36:49 +0700

    Add template

---
+++ E_learning/resources/views/index.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../asset/css/login.css">
+    <link rel="stylesheet" href="../lib/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="../lib/fontawesome/css/all.css">
+</head>
+<body>
+   <div id="container">
+        <div id="login">
+           <div class="header">
+                <div class="avt">
+                    <i class="far fa-user"></i>
+                </div>
+           </div>
+           <div class="text-center">Sign In</div>
+            <form action="#">
+                <div class="form-group">
+                    <input type="email" class="form-control" placeholder="Email" required name="email">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="Password" required name="password">
+                </div>
+                <button  class="button" type="submit">Login</button>
+                <!-- <a class="button" href="./student/index.html" type="submit">Login</a> -->
+            </form>
+            <div class="footer">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                    <label class="form-check-label text-primary" for="flexCheckChecked">
+                      Remember Me
+                    </label>
+                </div>
+                <div class="forgot-pass text-primary">
+                    <a href="/layouts/login/forgot-password.html">Forgot password?</a>
+                </div>  
+            </div>
+        </div>
+   </div>
+</body>
+</html>
---
+++ E_learning/resources/views/layouts/student/index.blade.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classroom</title>
+    <link rel="stylesheet" href="../asset/css/style.css">
+    <link rel="stylesheet" href="../lib/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="../lib/fontawesome/css/all.css">
+</head>
+<body>
+    <div id="header">
+        <div class="left">
+            <div class="drawer js-drawer">
+                <i class="fas fa-bars"></i>
+            </div>
+            <div class="logo">
+                <img src="../asset/img/googlelogo_clr_74x24px.svg" alt="">                   
+            </div>
+            <span class="webname">Lớp học</span>
+        </div>
+        <div class="right">
+            <div class="addclass">
+                <i class="fas fa-plus"></i>
+            </div>
+            <div class="menu">
+                <i class="fas fa-bars"></i>
+            </div>
+            <div class="account">
+                <i class="far fa-user-circle"></i>
+            </div>
+        </div>
+    </div>
+    <div class="dra-details">
+        <div class="dra-header">
+            <div class="dra-item">
+                <i class="fas fa-home icon"></i>
+                <a href="../student/index.html">Lớp học</a>
+            </div>
+            <div class="dra-item">
+                <i class="far fa-calendar icon"></i>
+                Lịch
+            </div>
+        </div>
+        <hr>
+        <div class="dra-body">
+            <div class="dra-body-title">Đã đăng ký</div>
+            <div class="dra-item">
+                <i class="far fa-list-alt icon"></i>
+                Việc cần làm
+            </div>
+            <div class="dra-item">
+                <div class="dra-item-avt icon">C</div>
+               CĐTH19PMC
+            </div>
+            <div class="dra-item">
+                <div class="dra-item-avt icon">C</div>
+                Lập trình web PHP
+            </div>
+            <div class="dra-item">
+                <div class="dra-item-avt icon">C</div>
+                ASP.NET Core MVC
+            </div>
+            <div class="dra-item">
+                <div class="dra-item-avt icon">C</div>
+                Lập trình di động
+            </div>
+            <div class="dra-item">
+                <div class="dra-item-avt icon">C</div>
+                Lập trình di động
+            </div>
+            <div class="dra-item">
+                <div class="dra-item-avt icon">C</div>
+                Lập trình di động
+            </div>
+            <div class="dra-item">
+                <div class="dra-item-avt icon">C</div>
+                Lập trình di động
+            </div>
+        </div>
+        <hr>
+        <div class="dra-footer">
+            <div class="dra-item">
+                <i class="far fa-save icon"></i>
+                Lớp học đã lưu trữ
+            </div>
+            <div class="dra-item">
+                <i class="fas fa-cog icon"></i>
+                Cài đặt
+            </div>
+        </div>
+    </div>
+    <div id="line"><hr></div>
+    <div id="container">      
+        <div id="body">
+            <div class="top">
+                <div class="work">
+                    <i class="far fa-list-alt"></i>
+                    <span>Việc cần làm</span>
+                </div>
+                <div class="calender">
+                    <i class="far fa-calendar-check"></i>
+                    <span>Lịch</span>
+                </div>
+            </div>
+            <div class="content row ">
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="left">
+                                <a class="title" href="class.html">Lập trình Web PHP Nâng Cao </a>
+                                <a class="subtitle" href="#">HK1 - NH 21-22 </a>
+                                <a href="#">Trần Thanh Tuấn</a>
+                            </div>
+                            <div class="right">
+                                <i class="fas fa-ellipsis-v"></i>
+                            </div>
+                            <div class="avt">
+
+                            </div>
+                        </div>
+                        <div class="card-body"></div>
+                        <div class="card-footer">
+                            <i class="fas fa-briefcase"></i>
+                            <i class="far fa-folder"></i>
+                        </div>
+                        </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="left">
+                                <a class="title" href="#">Lập trình Web PHP Nâng Cao </a>
+                                <a class="subtitle" href="#">HK1 - NH 21-22 </a>
+                                <a href="#">Trần Thanh Tuấn</a>
+                            </div>
+                            <div class="right">
+                                <i class="fas fa-ellipsis-v"></i>
+                            </div>
+                            <div class="avt"></div>
+                        </div>
+                        <div class="card-body"></div>
+                        <div class="card-footer">
+                            <i class="fas fa-briefcase"></i>
+                            <i class="far fa-folder"></i>
+                        </div>
+                        </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="left">
+                                <a class="title" href="#">Lập trình Web PHP Nâng Cao </a>
+                                <a class="subtitle" href="#">HK1 - NH 21-22 </a>
+                                <a href="#">Trần Thanh Tuấn</a>
+                            </div>
+                            <div class="right">
+                                <i class="fas fa-ellipsis-v"></i>
+                            </div>
+                            <div class="avt">
+
+                            </div>
+                        </div>
+                        <div class="card-body"></div>
+                        <div class="card-footer">
+                            <i class="fas fa-briefcase"></i>
+                            <i class="far fa-folder"></i>
+                        </div>
+                        </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="left">
+                                <a class="title" href="#">Lập trình Web PHP Nâng Cao </a>
+                                <a class="subtitle" href="#">HK1 - NH 21-22 </a>
+                                <a href="#">Trần Thanh Tuấn</a>
+                            </div>
+                            <div class="right">
+                                <i class="fas fa-ellipsis-v"></i>
+                            </div>
+                            <div class="avt">
+
+                            </div>
+                        </div>
+                        <div class="card-body"></div>
+                        <div class="card-footer">
+                            <i class="fas fa-briefcase"></i>
+                            <i class="far fa-folder"></i>
+                        </div>
+                        </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="left">
+                                <a class="title" href="#">Lập trình Web PHP Nâng Cao </a>
+                                <a class="subtitle" href="#">HK1 - NH 21-22 </a>
+                                <a href="#">Trần Thanh Tuấn</a>
+                            </div>
+                            <div class="right">
+                                <i class="fas fa-ellipsis-v"></i>
+                            </div>
+                            <div class="avt">
+
+                            </div>
+                        </div>
+                        <div class="card-body"></div>
+                        <div class="card-footer">
+                            <i class="fas fa-briefcase"></i>
+                            <i class="far fa-folder"></i>
+                        </div>
+                        </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="left">
+                                <a class="title" href="#">Lập trình Web PHP Nâng Cao </a>
+                                <a class="subtitle" href="#">HK1 - NH 21-22 </a>
+                                <a href="#">Trần Thanh Tuấn</a>
+                            </div>
+                            <div class="right">
+                                <i class="fas fa-ellipsis-v"></i>
+                            </div>
+                            <div class="avt">
+
+                            </div>
+                        </div>
+                        <div class="card-body"></div>
+                        <div class="card-footer">
+                            <i class="fas fa-briefcase"></i>
+                            <i class="far fa-folder"></i>
+                        </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="footer">
+
+        </div>
+    </div>
+
+    <script src="../asset/js/style.js"></script>
+</body>
+</html>
